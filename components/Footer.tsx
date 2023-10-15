7f48d5924092862942ef268e3d4de5343227d3d0
Kazunori Asari <asari@arthurs.co.jp>
2023-10-16 02:33:02 +0900

    update libraries

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ import { Text } from "@chakra-ui/react";
 export const Footer = () => {
   return (
     <footer>
-      <Text textAlign="center">© 2015-2022 Arthurs LLC</Text>
+      <Text textAlign="center">© 2015-2023 Arthurs LLC</Text>
     </footer>
   )
 }
--- components/__tests__/Footer.spec.tsx
+++ components/__tests__/Footer.spec.tsx
@@ -1,9 +1,9 @@
-import Footer from "../Footer";
+import { Footer } from "../Footer";
 import { render } from "@testing-library/react";
 
-describe("Sampleコンポーネント", () => {
-  test("should first", () => {
+describe("Footer", () => {
+  test("should have copyright text", () => {
     const { getByText } = render(<Footer />);
-    expect(getByText("© 2022 Arthurs LLC")).toBeTruthy();
+    expect(getByText("© 2015-2023 Arthurs LLC")).toBeTruthy();
   });
 });
